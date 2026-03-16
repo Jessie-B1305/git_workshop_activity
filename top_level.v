7df175eef68a60465546055efa178fffd02c72ee
Jessie-B1305 <jossie.bahr@gmail.com>
2026-03-16 19:09:13 +1100

    Update top_level.v

--- top_level.v
+++ top_level.v
@@ -8,16 +8,17 @@ module top_level (
 	input [1:0] KEY,
 		
 	// Leds
+	output [8:0] LED, // not sure if I have right allocations
 	
 	// Switches
-	
+	input [8:0] SW // not sure if I have right allocations
 );
 
     nios_system u0 (
-        .clk_clk       (/* Connect the clock */),       
+        .clk_clk       (CLOCK_50), /* Connect the clock */      
         .reset_reset_n (KEY[0]), /* Connect a Key for reset */
-        .ledr_export   (/* Connect all LEDs */),   
-        .sw_export     (/* Connect all Switches */)      
+        .ledr_export   (LED[8:0]), /* Connect all LEDs */  
+        .sw_export     (SW[8:0])  /* Connect all Switches */    
     );
 
 	 
